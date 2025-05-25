09d61ba4a0881f36153561915c55114c1d4dafef
Rangel Sousa <rangelgs17@gmail.com>
2025-05-25 16:16:47 -0300

    feat(migrations): adiciona primeira migração e cria tabela de usuários

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,18 @@
+from sqlalchemy import select
+
 from fast_zero.models import User
 
 
-def test_create_user():
-    user = User(username='Rangel', email='rangelgs17@gmail.com', password='minhasenha')
+def test_create_user(session):
+    user = User(
+        username='Rangel',
+        email='rangelgs17@gmail.com',
+        password='minhasenha',
+    )
 
-    assert user.username == 'Rangel'
+    session.add(user)
+    session.commit()
+
+    result = session.scalar(select(User).where(User.email == 'rangelgs17@gmail.com'))
+
+    assert result.username == 'Rangel'
